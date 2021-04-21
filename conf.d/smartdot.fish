ecfd59e05668118df440fa43d861c23121a752ff
Griffin Yourick <gryphon92@gmail.com>
2021-04-21 14:58:13 -0400

    Fix config to uninstall all functions correctly

--- conf.d/smartdot.fish
+++ conf.d/smartdot.fish
@@ -7,12 +7,12 @@ function _smartdot_fish_key_bindings --on-variable fish_key_bindings
         set --local mode default insert ||
         set --local mode insert default
 
-    bind --mode $mode[1] '.' _smartdot
+    bind --mode $mode[1] "." _smartdot
 end
 
 _smartdot_fish_key_bindings
 
 function _smartdot_uninstall --on-event _smartdot_uninstall
-    bind --erase .
-    functions --erase _smartdot
+    bind --erase "."
+    functions --erase (functions --all | string match "_smartdot*")
 end
